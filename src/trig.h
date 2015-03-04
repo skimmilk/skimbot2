9a061007a7c3dc867b299215f4b764ea04c7b17c
William DeGraaf <nswilliwa@gmail.com>
2015-03-04 04:28:23 -0600

    Fix trig utility class static functions

--- src/trig.h
+++ src/trig.h
@@ -21,15 +21,15 @@ class trig
 public:
 	// Offset your movement heading by amount
 	// Angles are NOT expressed in radians
-	void moveangle(float& forward, float& side, const float& angle, const float& y);
-	void moveangle(CUserCmd* cmd, const float& y);
+	static void moveangle(float& forward, float& side, const float& angle, const float& y);
+	static void moveangle(CUserCmd* cmd, const float& y);
 
 	// Difference between two angles
-	float diffangle(float, float);
+	static float diffangle(float, float);
 
 	// Set aim towards position in space
 	// If move is true, cmd's movement will be modified to keep moving forward
-	void aim(CUserCmd* cmd, const Vector& eyes, const Vector& at, bool move);
+	static void aim(CUserCmd* cmd, const Vector& eyes, const Vector& at, bool move);
 };
 
 } /* namespace skim */
